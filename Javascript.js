9baecc93fc38e33bd28c12cf5621d0fe40584857
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2022-12-14 22:45:50 -0500

    added Javascript

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,49 @@
-var todaysDate = dayjs().format('MMM D, YYYY');
+var todaysDate = dayjs().format('dddd, MMMM D, YYYY');
 $('#currentDay').text(todaysDate);
 
-var todaysTime = dayjs().format('h:mm:ss');
-$('#currentTime').text(todaysTime);
-
-function saveData() {
-    var input = document.getElementById("#hour");
-    localStorage.setItem("server", input.val());
+function saveText(event) {
+    var input = event.target.parentNode.id;
+    var text = event.target.parentNode.querySelector('textarea').value;
+    localStorage.setItem(input, text);
 }
-var storedValue = localStorage.getItem("server");
+addEventListener('DOMContentLoaded', (event) => {
+    for(var i = 1; i < 25; i++) {
+        var text = "hour-" + i;
+        var value = localStorage.getItem(text);
+        if(value) {
+            document.getElementById(text).querySelector('textarea').value = value;
+        }
+    }
+    updateTime();
+    setInterval(updateTime,1000);
+
+});
+function updateTime() {
+        var now = new Date();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds();
+        var time = hour + ":" + minute + ":" + second;
+document.getElementById("currentTime").innerHTML = time;
+        for(var i = 1; i < 25; i++) {
+            var text = "hour-" + i;
+            var container = document.getElementById(text);
+            if(!container) {
+                continue;
+            }
+            if(i < hour) {
+                container.classList.add("past");
+                container.classList.remove("present");
+                container.classList.remove("future");
+            } else if (i == hour) {
+                container.classList.add("present");
+                container.classList.remove("past");
+                container.classList.remove("future");
+            } else {
+                container.classList.add("future");
+                container.classList.remove("present");
+                container.classList.remove("past");
+            }
+        
+        }
+    }
